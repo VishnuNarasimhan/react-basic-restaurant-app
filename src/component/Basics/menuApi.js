5da8e3fda94cd2496b98fcd1d6898365b90dff39
VishnuNarasimhan <vishnunarasim20@gmail.com>
2022-06-05 06:50:24 +0530

    Restaurant App

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,102 @@
+const Menu = [
+    {
+        id:1,
+        image:"images/maggi.jpg",
+        name:"maggi",
+        category:"breakfast",
+        price:"12$",
+        description:"I love Maggie. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:2,
+        image:"images/sweet.jpg",
+        name:"sweet",
+        category:"dinner",
+        price:"5$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:3,
+        image:"images/corn.jpg",
+        name:"corn",
+        category:"breakfast",
+        price:"50$",
+        description:"I love Allu Pakoida. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:4,
+        image:"images/chola.jpg",
+        name:"chola",
+        category:"lunch",
+        price:"50$",
+        description:"I love Allu Pakoida. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:5,
+        image:"images/pizza.jpg",
+        name:"pizza",
+        category:"evening",
+        price:"80$",
+        description:"I love Allu Pakoida. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+    {
+        id:6,
+        image:"images/nonvegthali.jpg",
+        name:"Non-Veg Thali",
+        category:"lunch",
+        price:"10$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:7,
+        image:"images/puri.jpg",
+        name:"puri",
+        category:"dinner",
+        price:"10$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:8,
+        image:"images/rajmarice.jpg",
+        name:"rajmarice",
+        category:"lunch",
+        price:"10$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:9,
+        image:"images/rotiwithmeat.jpg",
+        name:"rotiwithmeat",
+        category:"dinner",
+        price:"10$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:10,
+        image:"images/samosa.jpg",
+        name:"samosa",
+        category:"evening",
+        price:"10$",
+        description:"I love Samosa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+
+    {
+        id:11,
+        image:"images/allupakoida.jpg",
+        name:"allupakodai",
+        category:"breakfast",
+        price:"50$",
+        description:"I love Allu Pakoida. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ducimus? Laudantium laboriosam quam aperiam veniam."
+    },
+]
+
+
+export default Menu;
